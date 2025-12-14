c992341783592f0b25bd645c0888467b44ff9624
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-12-14 17:41:37 +0000

    banner e intro

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Formulación y evaluación de aleaciones',
+    Description:
+      'El componente formativo permite comprender los principios que intervienen en la combinación de metales para obtener materiales con propiedades específicas y adecuadas para la fabricación de piezas de joyería. En este espacio se abordan los factores que influyen en la calidad de una aleación, y las condiciones de transformación que determinan su resistencia, color, durabilidad y facilidad de trabajo. A partir de este conocimiento, el aprendiz desarrolla criterios técnicos para analizar, seleccionar y valorar aleaciones según los requisitos del diseño y la función de cada pieza, fortaleciendo así su capacidad para garantizar productos finales estéticos, seguros y de alta calidad.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
@@ -12,6 +13,10 @@ export default {
       {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
       },
     ],
   },
@@ -31,28 +36,270 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Aleaciones',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Concepto',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Aleaciones',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Propiedades',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Composición',
+            hash: 't_1_4',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Ley de un metal precioso',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Concepto',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Clases',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Determinación',
+            hash: 't_2_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Kilate',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Concepto',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Equivalencias',
+            hash: 't_3_2',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Equipos y herramientas para fundición',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Descripción',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Clasificación',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: 'Mantenimiento',
+            hash: 't_4_3',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Fundición de metales',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: 'Concepto',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Técnicas',
+            hash: 't_5_2',
+          },
+          {
+            numero: '5.3',
+            titulo: 'Punto de fusión de metales',
+            hash: 't_5_3',
+          },
+          {
+            numero: '5.4',
+            titulo: 'Temperatura en metales para joyería',
+            hash: 't_5_4',
+          },
+          {
+            numero: '5.5',
+            titulo: 'Llamas oxidantes y reductoras',
+            hash: 't_5_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema6',
+        numero: '6',
+        titulo: 'Fundente',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '6.1',
+            titulo: 'Concepto',
+            hash: 't_6_1',
+          },
+          {
+            numero: '6.2',
+            titulo: 'Clases',
+            hash: 't_6_2',
+          },
+          {
+            numero: '6.3',
+            titulo: 'Preparación',
+            hash: 't_6_3',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema7',
+        numero: '7',
+        titulo: 'Colada del metal',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '7.1',
+            titulo: 'Concepto',
+            hash: 't_7_1',
+          },
+          {
+            numero: '7.2',
+            titulo: 'Técnicas',
+            hash: 't_7_2',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema8',
+        numero: '8',
+        titulo: 'Lingote',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '8.1',
+            titulo: 'Concepto',
+            hash: 't_8_1',
+          },
+          {
+            numero: '8.2',
+            titulo: 'Clases de lingotes',
+            hash: 't_8_2',
+          },
+          {
+            numero: '8.3',
+            titulo: 'Características',
+            hash: 't_8_3',
+          },
+          {
+            numero: '8.4',
+            titulo: 'Defectos en los lingotes',
+            hash: 't_8_4',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema9',
+        numero: '9',
+        titulo: 'Decapado',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '9.1',
+            titulo: 'Concepto',
+            hash: 't_9_1',
+          },
+          {
+            numero: '9.2',
+            titulo: 'Clases',
+            hash: 't_9_2',
+          },
+          {
+            numero: '9.3',
+            titulo: 'Técnicas',
+            hash: 't_9_3',
+          },
+          {
+            numero: '9.4',
+            titulo: 'Tipos de ácidos para decapado',
+            hash: 't_9_4',
+          },
+          {
+            numero: '9.5',
+            titulo: 'Cuidados en el uso de decapantes',
+            hash: 't_9_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema10',
+        numero: '10',
+        titulo: 'Pruebas de ley de los metales preciosos',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '10.1',
+            titulo: 'Concepto',
+            hash: 't_10_1',
+          },
+          {
+            numero: '10.2',
+            titulo: 'Tipos',
+            hash: 't_10_2',
+          },
+          {
+            numero: '10.3',
+            titulo: 'Aplicabilidad',
+            hash: 't_10_3',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema11',
+        numero: '11',
+        titulo: 'Normativa',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '11.1',
+            titulo: 'Seguridad y salud en el trabajo',
+            hash: 't_11_1',
+          },
+          {
+            numero: '11.2',
+            titulo: 'Riesgo 3',
+            hash: 't_11_2',
+          },
+        ],
       },
     ],
     subMenu: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -7,6 +7,22 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    .row.justify-content-center.align-items-center.mb-2
+      .col-sm-10.col-lg-5
+        figure
+          img.mb-4.mb-lg-0(src="@/assets/curso/intro/1.png" alt="Imagen de introducción al curso")
+      .col-lg-7
+        p El estudio y manejo de los metales preciosos constituye uno de los pilares fundamentales del oficio joyero, pues de su correcta selección, transformación y control dependen tanto la calidad estética como la integridad técnica de las piezas producidas.
+        p.mb-0 Este componente formativo orienta a fortalecer las competencias necesarias para comprender los principios fisicoquímicos de los metales, las técnicas de fundición y las normativas que regulan la pureza y estandarización de las aleaciones empleadas en la industria.
+  
+    .p-4.bg-brown.rounded-4.mb-5
+      .row.justify-content-center.align-items-center
+        .col-lg-9
+          p.mb-0 Además, también trata sobre aspectos como: formular aleaciones conforme a las técnicas de fundición y a los requerimientos específicos del cliente, lo que implica no solo que el aprendiz podrá dominar proporciones y reacciones metalúrgicas, sino también interpretar necesidades funcionales y estéticas. Asimismo, se aborda la habilidad de determinar la calidad de la aleación en concordancia con los estándares de ley y con las características finales del lingote, garantizando que el producto obtenido responda a parámetros de pureza, resistencia, color y comportamiento mecánico exigidos en la joyería profesional. Este componente formativo, por tanto, establece las bases técnicas y normativas necesarias para que el futuro joyero desarrolle procesos productivos seguros, precisos y alineados con las buenas prácticas del sector
+        .col-lg-2.d-none.d-lg-block
+          figure.mb-4.mb-md-0
+            img.w-138(src="@/assets/curso/intro/2.svg" alt="Imagen objetivo general")
     
     figure.mb-5
       .video.mb-2
