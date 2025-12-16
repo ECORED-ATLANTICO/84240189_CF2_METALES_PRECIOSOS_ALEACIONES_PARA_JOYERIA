a27df1428e048caee4a99d85e42c10d8edd4304f
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-12-16 22:05:22 +0000

    video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -26,8 +26,8 @@
     
     figure.mb-5
       .video.mb-2
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/kgUTwCV4OJA" title="Formulación y evaluación de aleaciones" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Formulación y evaluación de aleaciones
 
 </template>
 
